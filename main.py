b99772f5fefe44cf649889bab5629250a8585d0b
Bilal Ahmed <58539283+bilalahmed15@users.noreply.github.com>
2023-03-13 15:45:11 +0500

    app

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)#name of directory
+model = pickle.load(open('finalized_model.sav','rb'))
+
+#route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # for rendering results on html gui
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    return render_template('index.html', prediction_text="Employee Salary should be ${}".format(output))
+    
+if __name__ == "__main__":
+    app.run()
